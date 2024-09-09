141ab47b2719a990df4c18e61b179d57e2ac314c
palaparthy-k <keerthana.palaparthy@gmail.com>
2024-09-09 23:00:37 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import HomePage from './HomePage/HomePage.js';
+/*import {SmoothScrollHero} from './SmoothScrollHero/SmoothScrollHero.js';*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Keerthana Palaparthy</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
